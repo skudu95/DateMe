70026494f80f251aeb91f4bb04997ef4605ea5da
Kudu <kudu@Sharjeels-MacBook-Pro.local>
2022-10-22 15:24:49 +0600

    Initial commit

---
+++ app/src/main/java/com/kudu/dateme/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kudu.dateme.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kudu.dateme.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
